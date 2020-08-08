4626a705560d797302e0d21427a3eb8174c9d520
Aldhiya Rozak <32904416+Bejjoeqq@users.noreply.github.com>
2020-08-08 21:10:42 +0700

    2

--- opencv.py
+++ opencv.py
@@ -12,20 +12,19 @@ while True:
 	frame = cv2.cvtColor(img_np, cv2.COLOR_BGR2RGB)
 	width ,height = 500 , 500
 	imgResize = cv2.resize(frame,(width,height))
-	imgCropped = imgResize[130:155,180:207+y]
+	imgCropped = imgResize[135:153,180:207+y]
 	imCropResize  = cv2.resize(imgCropped,(imgResize.shape[1],imgResize.shape[0]))
 
 	a = cv2.cvtColor(imCropResize, cv2.COLOR_RGB2BGR)
-	print(a)
+	print(a.sum())
 	xx+=1
 	if xx%300==0:
 		y+=1
-	for x in a:
-		if np.array([83]) in x:
-			print("Jump")
-			key.press(" ")
-			time.sleep(0.3)
-			key.release(" ")
-			break
+	if 191250000 != a.sum():
+		print("Jump")
+		key.press(" ")
+		time.sleep(0.3)
+		key.release(" ")
+	cv2.imshow("",imCropResize)
 	if cv2.waitKey(1) == 27:
 		break
